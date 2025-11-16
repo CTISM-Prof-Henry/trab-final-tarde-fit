bbfa7cd664bc4c33c9746c3a23d4b99f8b4a4244
Igor Forner <igorfornerrodrigues2@gmail.com>
2025-11-16 17:14:50 -0300

    Adiciona rotas basicas

--- web-fit/src/app/app.routes.ts
+++ web-fit/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Body } from './components/body/body';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home",component: Body},
+];
